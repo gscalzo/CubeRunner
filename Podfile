09d4e54cd13ed0530b7330ee2efe5dc297253c03
giordano scalzo <giordano.scalzo@gmail.com>
2015-03-25 22:36:01 +0000

    first import

---
+++ Podfile
@@ -0,0 +1,8 @@
+inhibit_all_warnings!
+use_frameworks!
+
+target 'CubeRunner' do
+   pod 'Cartography', '~> 0.1'
+   pod 'HTPressableButton', '~> 1.3.1'
+   pod 'BitwiseFont', '~> 0.1.0'
+end
---
+++ Pods/Target Support Files/Pods-CubeRunner/Pods-CubeRunner-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// BitwiseFont
+#define COCOAPODS_POD_AVAILABLE_BitwiseFont
+#define COCOAPODS_VERSION_MAJOR_BitwiseFont 0
+#define COCOAPODS_VERSION_MINOR_BitwiseFont 1
+#define COCOAPODS_VERSION_PATCH_BitwiseFont 0
+
+// Cartography
+#define COCOAPODS_POD_AVAILABLE_Cartography
+#define COCOAPODS_VERSION_MAJOR_Cartography 0
+#define COCOAPODS_VERSION_MINOR_Cartography 2
+#define COCOAPODS_VERSION_PATCH_Cartography 1
+
+// HTPressableButton
+#define COCOAPODS_POD_AVAILABLE_HTPressableButton
+#define COCOAPODS_VERSION_MAJOR_HTPressableButton 1
+#define COCOAPODS_VERSION_MINOR_HTPressableButton 3
+#define COCOAPODS_VERSION_PATCH_HTPressableButton 1
+
+// HTPressableButton/All
+#define COCOAPODS_POD_AVAILABLE_HTPressableButton_All
+#define COCOAPODS_VERSION_MAJOR_HTPressableButton_All 1
+#define COCOAPODS_VERSION_MINOR_HTPressableButton_All 3
+#define COCOAPODS_VERSION_PATCH_HTPressableButton_All 1
+
